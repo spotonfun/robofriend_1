36730a1c0e69cce378a8b75aaa04266c685b630f
SpotOnFun <spotonfun@gmail.com>
2020-12-05 21:39:48 +0100

    working with search and styles

--- src/App.js
+++ src/App.js
@@ -2,15 +2,24 @@ import React from 'react';
 import CardList from './CardList'
 import {robots} from './robots'
 import SearchBox from './SearchBox'
+import './App.css'
+import Scroll from './Scroll'
 
 class App  extends React.Component {
 
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
-      robots : robots,
+      robots : [],
       searchfield: ''
     }
+  }
+
+  componentDidMount() { //no arrow function as it's part of react
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => {this.setState({robots: users})});
+    
   }
 
   onSearchChangeRRR = (event) => { //use such syntax to make sure "this" refers to App, not calling function
@@ -24,16 +33,21 @@ class App  extends React.Component {
       return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
     });
     // console.log(filterRobots);
-
-    return (
-      <div className="tc">
-        <h1>RoboFriend</h1>
-        <p>welcome to robofriend app</p>
-        <SearchBox searchChange={this.onSearchChangeRRR}/>
-        <CardList robots={filterRobots}/>
-      </div>
-    );
+    if(this.state.robots.length === 0) {
+      return <h1>Loading...</h1>
+    } else {
+      return (
+        <div className="tc">
+          <h1 className="f2">RoboFriend</h1>
+          <p>welcome to robofriend app</p>
+          <SearchBox searchChange={this.onSearchChangeRRR}/>
+          <Scroll>
+            <CardList robots={filterRobots}/>
+          </Scroll>
+        </div>
+      );
     }
+  }
 }
 
-export default App;
+export default App; 
